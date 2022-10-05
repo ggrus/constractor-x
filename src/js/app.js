cc224b6b8473ec8896a66ea388fe5cde0b8d8074
Ruslan <grr_95@mail.ru>
2022-10-05 16:06:19 +0300

    for work

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,23 @@
-console.log(`'it's work!`);
+const hamburgerBtn = document.querySelector('.hamburger-btn')
+const headerMenu = document.querySelector('.menu')
+
+hamburgerBtn.onclick = () => {
+  hamburgerBtn.classList.toggle('hamburger-btn--open')
+  headerMenu.classList.toggle('menu--open')
+  document.body.classList.toggle('_lock')
+}
+
+// Header
+const headerElement = document.querySelector('.header');
+
+const callback = function (entries, observer) {
+  console.log(entries);
+  if (entries[0].isIntersecting) {
+    headerElement.classList.remove('scroll');
+  } else {
+    headerElement.classList.add('scroll');
+  }
+};
+
+const headerObserver = new IntersectionObserver(callback);
+headerObserver.observe(headerElement);
